4c981f9eb2abff3813a0b492da8b8d313b0a60a6
jorasdf24 <jorasdf24@gmail.com>
2025-07-07 00:00:07 +0300

    Update 1.0

---
+++ bmiCalculator.ts
@@ -0,0 +1,14 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const bmiValue = weight / (height * height);
+  switch (bmiValue) {
+    case bmiValue > :
+        
+        break;
+  
+    default:
+        break;
+  }
+
+
+
+};
